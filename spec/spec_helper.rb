3724a69b8c40bec0863129d9b4903b2dc8078853
nov <nov@matake.jp>
2022-10-02 09:03:29 +0900

    up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,10 @@ if ENV['GITHUB_ACTIONS']
   require 'simplecov-lcov'
   SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
   SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
-  SimpleCov.start
-else
-  SimpleCov.start do
-    add_filter 'spec'
-  end
+end
+
+SimpleCov.start do
+  add_filter 'spec'
 end
 
 require 'rspec'
